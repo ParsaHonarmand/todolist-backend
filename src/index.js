328fc1f79f29ce443b5c2055b8d15b5c7d536c16
parsa Honarmand <parsa.Honarmand@gmail.com>
2020-01-03 16:46:05 -0700

    more functionality

--- src/index.js
+++ src/index.js
@@ -61,8 +61,8 @@ app.post('/item', (req, res) => {
     // collection.insertOne(req.body)
     //     .then(res => console.log(res.ops[0]))
     //     .catch(err => console.log(err))
-    collection.updateOne({username: req.body.username}, {$set: {"todos": req.body.todo}})
-        .then(res => console.log("added to todolist"))
+    collection.updateOne({"username": req.body.username}, {$set: {"todos": req.body.todo}})
+        .then(res => console.log("added to todolist", res.data))
         .catch(err => console.log(err))
 });
 
